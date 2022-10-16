01b433bd2213f8dc56ed754680714f82795323df
FelipeOthon <felipeosantiago@gmail.com>
2022-10-16 14:07:35 -0300

    enviar exercicio

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int horas;
+
+        System.out.println("Que horas são?");
+        horas = scanner.nextInt();
+
+        if (horas < 12)
+
+            System.out.println("Bom dia!");
+
+        else if (horas < 18) {
+            System.out.println("Bom tarde!");
+        } else {
+            System.out.println("Bom noite!");
+        }
+    }
+}
+
+
+
